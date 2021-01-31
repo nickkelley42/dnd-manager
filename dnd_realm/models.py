3ad5ced65023291dbb22aa49a4400840328e4eb0
Nick Kelley <nick@nickkelleystuff.com>
2021-01-31 15:29:26 -0800

    basic realm/character relationship

--- dnd_realm/models.py
+++ dnd_realm/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Realm(models.Model):
+    name = models.CharField(max_length=30)
+
+class Character(models.Model):
+    name = models.CharField(max_length=30, default="")
+
+    realm = models.ForeignKey(Realm, on_delete=models.SET_NULL, null=True)
+
+    base_strength = models.IntegerField(default=0)
+    base_dexterity = models.IntegerField(default=0)
+    base_constitution = models.IntegerField(default=0)
+    base_intelligence = models.IntegerField(default=0)
+    base_wisdom = models.IntegerField(default=0)
+    base_charisma = models.IntegerField(default=0)
--- dnd_realm/tests.py
+++ dnd_realm/tests.py
@@ -1,3 +1,57 @@
 from django.test import TestCase
+from .models import Realm, Character
 
 # Create your tests here.
+
+class RealmModelTests(TestCase):
+    def test_realm_model_exists(self):
+        """
+        the realm model should exist and be instantiatable.
+        """
+        realm = Realm()
+        self.assertIsInstance(realm, Realm)
+
+    def test_realm_model_has_name(self):
+        """
+        The realm model should have a name string attribute
+        """
+        realm = Realm()
+        self.assertIsInstance(realm.name, str)
+
+    def test_realm_has_many_characters(self):
+        """
+        A realm may have multiple associated characters
+        """
+        realm = Realm()
+        realm.save()
+        for i in range(3):
+            realm.character_set.create()
+        self.assertEqual(len(realm.character_set.all()), 3)
+
+
+class CharacterModelTests(TestCase):
+    def test_character_model_exists(self):
+        """
+        Character model should exist
+        """
+        character = Character()
+        self.assertIsInstance(character, Character)
+
+    def test_character_model_has_name(self):
+        """
+        Characters must have a name
+        """
+        character = Character()
+        self.assertIsInstance(character.name, str)
+
+    def test_character_has_base_abilities(self):
+        """
+        Character models have base ability scores.
+        """
+        character = Character()
+        self.assertIsInstance(character.base_strength, int)
+        self.assertIsInstance(character.base_dexterity, int)
+        self.assertIsInstance(character.base_constitution, int)
+        self.assertIsInstance(character.base_intelligence, int)
+        self.assertIsInstance(character.base_wisdom, int)
+        self.assertIsInstance(character.base_charisma, int)
